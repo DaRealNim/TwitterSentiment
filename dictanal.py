3616fb93811b8be8476bddeb8a5b6b3fa6d6eaad
DaRealNim <darealnim@gmail.com>
2022-05-07 13:38:19 +0200

    formatted annotated dict and raw scoring test

--- dictanal.py
+++ dictanal.py
@@ -4,30 +4,36 @@ import os
 import pickle
 
 def getNegAndPosWords():
-    with open("dicts/frlsd.cat") as dictFile:
+    with open("dicts/annotated.dict") as dictFile:
         lines = iter(dictFile.readlines())
-        negativeWords = []
-        positiveWords = []
-
+        negativeWords = {}
+        positiveWords = {}
         next(lines)
-        while ((neg := next(lines).strip()) != "POSITIVE"):
-            neg = neg.split("(")
-            neg[0] = neg[0].strip()
-            neg[1] = neg[1].replace(")", "")
-            neg[1] = int(neg[1])
-            negativeWords.append(neg)
-    
-        while True:
-            try:
-                pos = next(lines).strip()
-                pos = pos.split("(")
-                pos[0] = pos[0].strip() 
-                pos[1] = pos[1].replace(")","")
-                pos[1] = int(pos[1])           
-                positiveWords.append(pos)
-                print(pos)
-            except StopIteration:
-                break
+        c = 2
+        try:
+            while ((neg := next(lines).strip()) != "POSITIVE"):
+                neg = neg.split("(")
+                neg[0] = neg[0].strip()
+                neg[1] = neg[1].replace(")", "")
+                neg[1] = float(neg[1])
+                negativeWords[neg[0]] = neg[1]
+                c += 1
+                
+        
+            while True:
+                try:
+                    pos = next(lines).strip()
+                    c += 1
+                    pos = pos.split("(")
+                    pos[0] = pos[0].strip() 
+                    pos[1] = pos[1].replace(")","")
+                    pos[1] = float(pos[1])           
+                    positiveWords[pos[0]] = pos[1]
+                except StopIteration:
+                    break
+        except Exception as e:
+            print(e)
+            print("line %d"%c)
     dictFile.close()
 
     return (positiveWords, negativeWords)
@@ -43,17 +49,45 @@ def openTweets():
     
     return (haineTweets, bonheurTweets)
 
-nlp = stanza.Pipeline(lang='fr', processors='tokenize,sentiment', dir=os.getenv("DATA_DIR"))
 
+nlp = stanza.Pipeline(lang='fr', processors='tokenize', dir=os.getenv("DATA_DIR"))
+
+
+f = open("exemple_datasets/haine.lst", "rb")
+haineTweets = pickle.load(f)
+f.close()
+
+f = open("exemple_datasets/bonheur.lst", "rb")
+bonheurTweets = pickle.load(f)
+f.close()
 
 posWords, negWords = getNegAndPosWords()
 
-score = 0
+hatescore = 0
 
 for tweet in haineTweets:
     doc = nlp(tweet["text"])
-    for word in doc.sentences:
-        if (word in negWords):
-            score -= 1
-        if (word in posWords):
-            score += 1
+    for sentence in doc.sentences:
+        for word in sentence.words:
+            if (word.text.upper() in negWords):
+                hatescore -= negWords[word.text.upper()]
+            if (word.text.upper() in posWords):
+                hatescore += posWords[word.text.upper()]
+
+print("hate tweets score : %f"%hatescore)
+
+happyscore = 0
+
+for tweet in bonheurTweets:
+    doc = nlp(tweet["text"])
+    for sentence in doc.sentences:
+        for word in sentence.words:
+            if (word.text.upper() in negWords):
+                happyscore -= negWords[word.text.upper()]
+            if (word.text.upper() in posWords):
+                happyscore += posWords[word.text.upper()]
+
+print("happiness tweets score : %f"%happyscore)
+
+
+
